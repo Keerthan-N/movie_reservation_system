2d4cc7811345e971b5d70a68d2f5c0a05579621e
Keerthan N <110042672+Keerthan-N@users.noreply.github.com>
2025-06-26 10:34:01 +0530

    Add files via upload

---
+++ src/main/java/com/project/movie/MovieReservationApiApplication.java
@@ -0,0 +1,13 @@
+package com.project.movie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieReservationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieReservationApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/movie/MovieReservationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.movie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieReservationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
